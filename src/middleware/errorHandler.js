f13c1db39d2638c667032a42e5868cc1d8cc7dc8
Твоє Ім'я <korostelovanatalia66@gmail.com>
2026-03-12 23:13:39 +0200

    F

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,11 @@
+import createHttpError from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
 
-  const status = err.status || 500;
+  const status = createHttpError.isHttpError(err) ? err.status : 500;
   const message = err.message || 'Internal Server Error';
 
   res.status(status).json({ message });
