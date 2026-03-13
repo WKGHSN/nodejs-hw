6261d004a44fb22ded88e35d7a309829a4505ca7
Твоє Ім'я <korostelovanatalia66@gmail.com>
2026-03-13 23:31:06 +0200

    D

--- src/server.js
+++ src/server.js
@@ -20,11 +20,10 @@ app.use(logger);
 app.use(cors());
 app.use(express.json());
 
-app.use('/notes', notesRoutes);
 
+app.use(notesRoutes);
 
 app.use(errors());
-
 app.use(notFoundHandler);
 app.use(errorHandler);
 
