a13444f6b22bc12d03f45ede0c8e9f7cecfe7412
Твоє Ім'я <korostelovanatalia66@gmail.com>
2026-03-18 19:14:15 +0200

    G

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,8 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
-
 export const updateUserAvatar = async (req, res, next) => {
   try {
-
     if (!req.file) {
       throw createHttpError(400, 'No file');
     }
@@ -15,7 +13,7 @@ export const updateUserAvatar = async (req, res, next) => {
     const user = await User.findByIdAndUpdate(
       req.user._id,
       { avatar: result.secure_url },
-      { new: true }
+      { returnDocument: 'after' }
     );
 
     res.status(200).json({
